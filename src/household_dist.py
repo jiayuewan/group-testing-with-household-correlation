66a4244864edf679443afc527a7c65590aa449f6
Jiayue <wanjiayue.jason@gmail.com>
2020-12-30 15:27:54 -0500

    update test for generate infection states

--- src/household_dist.py
+++ src/household_dist.py
@@ -1,3 +1,9 @@
+"""
+Household distributions for different countries:
+percentage of households that consists of 1, 2, 3, 4, 5, 6+ individuals
+"""
+
+
 US_DIST = [0.284, 0.345, 0.151, 0.127, 0.058, 0.035]
 CN_DIST = [0.156, 0.272, 0.247, 0.171, 0.089, 0.065]
 AUS_DIST = [0.244, 0.334, 0.162, 0.159, 0.067, 0.034]
