41c8b8b5a12c4234b0ea291163db2ed3c409f9f7
Jiayue <wanjiayue.jason@gmail.com>
2021-01-18 19:48:15 -0500

    add tmp pareto plots and heatmap for experiment 2

--- src/plotting_helpers.py
+++ src/plotting_helpers.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 import pandas as pd
+#import seaborn as sns
+import re
 
 
 def plot_hist_exp_1(results, household_size, pool_size, prevalence):
@@ -100,4 +102,77 @@ def generate_sensitivity_plots(param):
 
 
 def generate_pareto_fontier_plots():
+    dir = '../results/experiment_2/pareto_analysis/'
+
+    aggregate_results = {}
+
+    for filename in os.listdir(dir):
+        if filename == ".DS_Store":
+            continue
+
+        parts = re.split('=|[.](?!\d)|_', filename)
+        prev = float(parts[2])
+        pool_size = int(parts[4])
+
+        filedir = os.path.join(dir, filename)
+        with open(filedir) as f:
+            results = np.loadtxt(f)
+
+        avgs = np.mean(results, axis=0)
+        aggregate_results[(prev, pool_size)] = avgs
+
+    df_agg = pd.DataFrame.from_dict(aggregate_results, orient='index', columns=['indep fnr', 'corr fnr', 'indep eff', 'corr eff'])
+    df_agg.index = pd.MultiIndex.from_tuples(df_agg.index, names=['prevalence', 'pool size'])
+    df_agg = df_agg.reset_index()
+    df_agg = df_agg.sort_values(by=['prevalence', 'pool size'])
+
+    for prev in df_agg['prevalence'].unique():
+        df = df_agg[df_agg['prevalence'] == prev]
+        ax = df.sort_values(by='pool size').plot(x='indep fnr', y = 'indep eff', sort_columns=True)
+        df.sort_values(by='pool size').plot(x='corr fnr', y = 'corr eff', sort_columns=True, ax=ax)
+
+        for i, point in df.iterrows():
+            ax.text(point['indep fnr'], point['indep eff'], str(int(point['pool size'])))
+            ax.text(point['corr fnr'], point['corr eff'], str(int(point['pool size'])))
+        plt.legend(['indep', 'corr'])
+        plt.xlabel('fnr')
+        plt.ylabel('eff')
+        plt.title('eff vs fnr, indep vs corr for prevalence={}'.format(prev))
+
+        plt.savefig('../figs/experiment_2/pareto_plots/tmp_pareto_for_prev_{}.pdf'.format(prev))
+        plt.close()
+
+    df_agg['fnr diff'] = df_agg['indep fnr'] - df_agg['corr fnr']
+    df_agg['eff diff'] = df_agg['corr eff'] - df_agg['indep eff']
+
+    table_fnr = pd.pivot_table(df_agg, values='fnr diff', index=['prevalence'], columns=['pool size'])
+    heatmap = plt.pcolor(table_fnr)
+    plt.yticks(np.arange(0.5, len(table_fnr.index), 1), table_fnr.index)
+    plt.xticks(np.arange(0.5, len(table_fnr.columns), 1), table_fnr.columns)
+    plt.xlabel('pool size')
+    plt.ylabel('prevalence')
+    plt.title('heat map for (indep fnr - corr fnr)')
+    plt.colorbar(heatmap)
+    plt.savefig('../figs/experiment_2/pareto_plots/tmp_heapmap_for_fnr.pdf')
+    plt.close()
+
+    table_eff = pd.pivot_table(df_agg, values='eff diff', index=['prevalence'], columns=['pool size'])
+    heatmap = plt.pcolor(table_eff)
+    plt.yticks(np.arange(0.5, len(table_eff.index), 1), table_eff.index)
+    plt.xticks(np.arange(0.5, len(table_eff.columns), 1), table_eff.columns)
+    plt.xlabel('pool size')
+    plt.ylabel('prevalence')
+    plt.title('heat map for (corr eff - indep eff)')
+    plt.colorbar(heatmap)
+    plt.savefig('../figs/experiment_2/pareto_plots/tmp_heapmap_for_eff.pdf')
+    plt.close()
     return
+
+
+if __name__ == '__main__':
+    #run_simulations_for_sensitivity_analysis()
+
+    # for param in ['prevalence', 'pool size', 'SAR', 'FNR', 'household dist']:
+    #     generate_sensitivity_plots(param)
+
+    generate_pareto_fontier_plots()
