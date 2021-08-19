c8bcd050d953de2a5e03a373c3d3852c690a06b7
Jiayue Wan <wanjiayue.jason@gmail.com>
2021-08-19 10:15:30 -0400

    update plots for nominal scenario and sensitivity analyses using 2k iterations

---
+++ scripts/test.sh
@@ -0,0 +1,2 @@
+cd /home/jw2529/group-testing-with-household-correlation/scripts/
+/home/jw2529/.conda/envs/jw2529/bin/python test.py
---
+++ scripts/test2.py
@@ -0,0 +1,42 @@
+# from multiprocessing import Process
+# import os, time, random, sys
+
+# def long_time_task(name):
+#     print('Run task %s (%s)...' % (name, os.getpid()))
+#     start = time.time()
+#     time.sleep(random.random() * 3)
+#     end = time.time()
+#     print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+#     # sys.stdout.flush()
+
+# if __name__=='__main__':
+#     print('Parent process %s.' % os.getpid())
+#     jobs = []
+#     for i in range(30):
+#         p = Process(target=long_time_task, args=(i,))
+#         jobs.append(p)
+#         p.start()
+#     print('Waiting for all subprocesses done...')
+#     for p in jobs:
+#         p.join()
+#     print('All subprocesses done.')
+
+from multiprocessing import Pool
+import os, time, random
+
+def long_time_task(name):
+    print('Run task %s (%s)...' % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+
+if __name__=='__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Pool(4)
+    for i in range(5):
+        p.apply_async(long_time_task, args=(i,))
+    print('Waiting for all subprocesses done...')
+    p.close()
+    p.join()
+    print('All subprocesses done.')
