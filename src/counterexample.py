b0dc8cd4a85aed96c74d4ab48ecbc24e89dcdd54
zyyjjj <yz685@cornell.edu>
2021-10-26 16:54:57 -0400

    add code and pdf plot of counterexample efficiency heat map

--- src/counterexample.py
+++ src/counterexample.py
@@ -36,27 +36,15 @@ def plot_diff_efficiency():
     fig, ax = plt.subplots()
     im = ax.imshow(data, cmap = cmap, extent = [0, 100, 100, 0])
     cbar = ax.figure.colorbar(im, ax=ax)
-
-    mask = data < 0
-    for j, i in np.column_stack(np.where(mask)):
-        ax.add_patch(
-            mpatches.Rectangle(
-                (i, j),     # (x,y)
-                1,          # width
-                1,          # height
-                fill=True, 
-                color='purple',
-                snap=False,
-                alpha = 0.1,
-                hatch='/' # the more slashes, the denser the hash lines 
-            ))
         
     ax.set_xlabel(r'$\theta_1$')
     ax.set_ylabel(r'$\theta_2$')
     ax.set_title("Difference in efficiency \n between correlated and naive pooling")
     fig.tight_layout()
     #plt.show()
-    plt.contour(data, levels = [0], colors = 'purple', alpha = 0.7)
+    plt.contour(data, levels = [0], colors = 'r', linewidth=5)
+    ax.hlines(y=100, xmin=0, xmax=49, color='r', linestyle='-', linewidth=5)
+    ax.vlines(x=0.1, ymin=2, ymax=100, color='r', linestyle='-', linewidth=5)
 
 
     plt.savefig('/home/yz685/group-testing-with-household-correlation/figs/counterexample.pdf', format='pdf', dpi=600, bbox_inches='tight')
