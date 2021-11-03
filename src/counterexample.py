2f6541fd1d7f018e010d8684638fb00c1ab9cdbf
zyyjjj <yz685@cornell.edu>
2021-11-03 11:49:04 -0400

    update counterexample calc and plot

--- src/counterexample.py
+++ src/counterexample.py
@@ -7,19 +7,37 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
 
-beta1_list = np.linspace(0.01, 1, 100)
-beta2_list = np.linspace(0.01, 1, 100)
+theta1_list = np.linspace(0.01, 1, 100)
+theta2_list = np.linspace(0.01, 1, 100)
+num_grid = len(theta1_list)
 n=2
 alpha=0.01
 
-num_grid = len(beta1_list)
+num_grid = len(theta1_list)
 
 
-def diff_efficiency(beta1, beta2, n=n, alpha=alpha):
-  np_val_inv = 1/n + alpha * n * 12/(7*beta2+5)*(7*beta1*beta2/12 + 5*beta1/12)
-  cp_val_inv = 1/n + alpha * n * (8*beta1+2*beta2)/(5*beta1+3*beta1*beta2+4*beta2**2)*(beta1*beta2/4 + beta2**2/3 + 5*beta1/12)
+def compute_np_beta(alpha, theta1, theta2):
 
-  return 1/cp_val_inv - 1/np_val_inv # positive means cp is better
+    return (1 - alpha * (7/12*theta2**2 + 7/12*5/12*theta2 + 5**2/12**2) \
+            - (1-alpha) * (7/12*theta1*theta2+5/12*theta1))
+
+def compute_np_eta(alpha, theta1, theta2, np_beta, n=2):
+
+    denom = n * alpha * (1-np_beta)
+    num = n * (theta1 * 2 * alpha * (1-alpha)\
+                + theta2 * alpha**2 * ((7/12)**2 + 2 * 7/12 * 5/12)\
+                + alpha**2 * (5/12)**2)
+  
+    return num/denom
+
+def compute_np_efficiency(n, alpha, eta, beta):
+    inv_eff = 1/n + alpha * eta * (1-beta)
+    return 1/inv_eff
+
+def compute_cp_efficiency(n, alpha, theta1, theta2):
+    cp_inv_eff = 1/n + alpha * n * (8*theta1+2*theta2)/(5*theta1+3*theta1*theta2+4*theta2**2)*(theta1*theta2/4 + theta2**2/3 + 5*theta1/12)
+
+    return 1/cp_inv_eff
 
 
 def plot_diff_efficiency():
@@ -29,7 +47,13 @@ def plot_diff_efficiency():
 
     for i in range(num_grid):
         for j in range(i, num_grid):
-            diff_eff_vals[i,j] = diff_efficiency(beta1_list[i], beta2_list[j], n, alpha)    
+
+            np_beta = compute_np_beta(alpha, theta1_list[i], theta2_list[j])
+            np_eta = compute_np_eta(alpha, theta1_list[i], theta2_list[j], np_beta)
+            np_eff = compute_np_efficiency(n, alpha, np_eta, np_beta)
+            cp_eff = compute_cp_efficiency(n, alpha, theta1_list[i], theta2_list[j])
+
+            diff_eff_vals[i,j] = cp_eff - np_eff
 
     data = diff_eff_vals.transpose()
 
