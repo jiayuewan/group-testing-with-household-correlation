6e358bee2f56292ab9d27ca71092a9505a452769
jiayuewan <wanjiayue.jason@gmail.com>
2021-11-14 19:10:52 -0500

    deletes script directory

--- scripts/corr_pool_sim_2000.sh
+++
@@ -1,3 +0,0 @@
-cd /home/jw2529/group-testing-with-household-correlation/src/
-/home/jw2529/.conda/envs/jw2529/bin/python experiment_2.py 2000
-/home/jw2529/.conda/envs/jw2529/bin/python plotting_helpers.py
--- scripts/lod_to_fnr.sh
+++
@@ -1,2 +0,0 @@
-cd /home/jw2529/group-testing-with-household-correlation/src/
-/home/jw2529/.conda/envs/jw2529/bin/python PCR_test.py
