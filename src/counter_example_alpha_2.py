27d31b10615f93b9e506aa363378ec2c65154434
zyyjjj <yz685@cornell.edu>
2021-11-14 21:02:26 -0500

    updated figs for EC1

--- src/counter_example_alpha_2.py
+++ src/counter_example_alpha_2.py
@@ -34,8 +34,8 @@ def compute_cp_beta(alpha, theta1, theta2):
     return res
 
 
-def compute_cp_eta(alpha, theta1, theta2, np_beta, n=2):
-    denom = n * alpha * (1 - np_beta)
+def compute_cp_eta(alpha, theta1, theta2, cp_beta, n=2):
+    denom = n * alpha * (1 - cp_beta)
     num = n * alpha * (theta1 + 1 / 2 * theta2)
     return num / denom
 
@@ -43,6 +43,9 @@ def compute_cp_eta(alpha, theta1, theta2, np_beta, n=2):
 def compute_efficiency(n, alpha, eta, beta):
     inv_eff = 1 / n + alpha * eta * (1 - beta)
     return 1 / inv_eff
+
+def compute_lim_eta_ratio(theta1, theta2):
+    return (2*theta1+theta2)*(theta2+1) / ( (theta1+theta2**2) * 4 )
 
 
 def plot_diff_efficiency():
@@ -79,7 +82,8 @@ def plot_diff_efficiency():
     ax.set_yticklabels(np.linspace(0, 100, 6) / 100)
     ax.set_xlabel(r'$\theta_1$')
     ax.set_ylabel(r'$\theta_2$')
-    ax.set_title("Difference in efficiency \n between correlated and naive pooling")
+    ax.set_title(r"${efficiency}_{1,\alpha} - {efficiency}_{0,\alpha}$")
+    ax.text(12, 60, 'A', color='white')
     fig.tight_layout()
     #plt.show()
     plt.contour(data, levels = [0], colors = 'r', linewidth=5)
@@ -88,27 +92,98 @@ def plot_diff_efficiency():
 
     plt.savefig('../figs/counterexample_alpha_over_2.pdf', format='pdf', dpi=600, bbox_inches='tight')
 
+def plot_beta_ratio():
 
-def plot_eta_ratio_minus_one():
-
-    delta_vals = np.empty((num_grid, num_grid))
-    delta_vals[:] = np.nan
+    beta_vals = np.empty((num_grid, num_grid))
+    beta_vals[:] = np.nan
     for i in range(num_grid):
         for j in range(i, num_grid):
             theta1 = theta1s[i]
             theta2 = theta2s[j]
 
             np_beta = compute_np_beta(alpha, theta1, theta2)
+            cp_beta = compute_cp_beta(alpha, theta1, theta2)
+            
+            beta_vals[i,j] = cp_beta - np_beta
+
+
+    data = beta_vals.transpose()
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(data, cmap = cmap, extent = [0, 100, 100, 0])
+    cbar = ax.figure.colorbar(im, ax=ax)
+    ax.set_xlim(-0.3, 100)
+    ax.set_xticks(np.linspace(0, 100, 6))
+    ax.set_xticklabels(np.linspace(0, 100, 6) / 100)
+    ax.set_yticks(np.linspace(0, 100, 6))
+    ax.set_yticklabels(np.linspace(0, 100, 6) / 100)
+    ax.set_xlabel(r'$\theta_1$')
+    ax.set_ylabel(r'$\theta_2$')
+    ax.set_title(r"${\beta_{1,\alpha}} \ -\  {\beta_{0,\alpha}}$")
+    #ax.text(1, 18, 'B', color='white')
+    fig.tight_layout()
+    #plt.show()
+    plt.contour(data, levels = [0], colors = 'r', linewidth=5)
+    #ax.hlines(y=100, xmin=0, xmax=49, color='r', linestyle='-', linewidth=5)
+    #ax.vlines(x= -0.2, ymin=0, ymax=45, color='r', linestyle='-', linewidth=2)
+
+    plt.savefig('../figs/counterexample_alpha_over_2_beta.pdf', format='pdf', dpi=600, bbox_inches='tight')
+
+
+def plot_eta_ratio():
+
+    ratio_vals = np.empty((num_grid, num_grid))
+    ratio_vals[:] = np.nan
+    for i in range(num_grid):
+        for j in range(i, num_grid):
+            theta1 = theta1s[i]
+            theta2 = theta2s[j]
+
+            #ratio_vals[i,j] = compute_lim_eta_ratio(theta1, theta2) 
+            np_beta = compute_np_beta(alpha, theta1, theta2)
             np_eta = compute_np_eta(alpha, theta1, theta2, np_beta)
-            #np_eff = compute_efficiency(n, alpha, np_eta, np_beta)
 
             cp_beta = compute_cp_beta(alpha, theta1, theta2)
             cp_eta = compute_cp_eta(alpha, theta1, theta2, cp_beta)
-            #cp_eff = compute_efficiency(n, alpha, cp_eta, cp_beta)
             
-            delta_vals[i,j] = cp_eta / np_eta - 1
+            ratio_vals[i,j] = cp_eta / np_eta
 
-    data = delta_vals.transpose()
+
+    data = ratio_vals.transpose()
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(data, cmap = cmap, extent = [0, 100, 100, 0])
+    cbar = ax.figure.colorbar(im, ax=ax)
+    ax.set_xlim(-0.3, 100)
+    ax.set_xticks(np.linspace(0, 100, 6))
+    ax.set_xticklabels(np.linspace(0, 100, 6) / 100)
+    ax.set_yticks(np.linspace(0, 100, 6))
+    ax.set_yticklabels(np.linspace(0, 100, 6) / 100)
+    ax.set_xlabel(r'$\theta_1$')
+    ax.set_ylabel(r'$\theta_2$')
+    ax.set_title(r"${\eta_{1,\alpha}} \ /\  {\eta_{0,\alpha}}$")
+    ax.text(1, 18, 'B', color='white')
+    fig.tight_layout()
+    #plt.show()
+    plt.contour(data, levels = [1], colors = 'r', linewidth=5)
+    #ax.hlines(y=100, xmin=0, xmax=49, color='r', linestyle='-', linewidth=5)
+    ax.vlines(x= -0.2, ymin=0, ymax=45, color='r', linestyle='-', linewidth=2)
+
+    plt.savefig('../figs/counterexample_alpha_over_2_eta.pdf', format='pdf', dpi=600, bbox_inches='tight')
+
+
+def plot_lim_eta_ratio():
+
+    ratio_vals = np.empty((num_grid, num_grid))
+    ratio_vals[:] = np.nan
+    for i in range(num_grid):
+        for j in range(i, num_grid):
+            theta1 = theta1s[i]
+            theta2 = theta2s[j]
+
+            ratio_vals[i,j] = compute_lim_eta_ratio(theta1, theta2) 
+
+    data = ratio_vals.transpose()
 
     fig, ax = plt.subplots()
     im = ax.imshow(data, cmap = cmap, extent = [0, 100, 100, 0])
@@ -119,14 +194,14 @@ def plot_eta_ratio_minus_one():
     ax.set_yticklabels(np.linspace(0, 100, 6) / 100)
     ax.set_xlabel(r'$\theta_1$')
     ax.set_ylabel(r'$\theta_2$')
-    ax.set_title(r"$\eta_{1,\alpha} / \eta_{0,\alpha} - 1$")
+    ax.set_title(r"$\lim_{\alpha\to 0^+} \ \dfrac{\eta_{1,\alpha}}{\eta_{0,\alpha}}$")
     fig.tight_layout()
     #plt.show()
-    plt.contour(data, levels = [0], colors = 'r', linewidth=5)
+    plt.contour(data, levels = [1], colors = 'r', linewidth=5)
     #ax.hlines(y=100, xmin=0, xmax=49, color='r', linestyle='-', linewidth=5)
     #ax.vlines(x=0, ymin=0, ymax=43, color='r', linestyle='-', linewidth=5)
 
-    plt.savefig('../figs/counterexample_alpha_over_2_eta.pdf', format='pdf', dpi=600, bbox_inches='tight')
+    plt.savefig('../figs/counterexample_alpha_over_2_eta_limit.pdf', format='pdf', dpi=600, bbox_inches='tight')
 
 
 
@@ -134,4 +209,23 @@ if __name__ == '__main__':
     plt.rcParams["font.family"] = 'serif'
     
     #plot_diff_efficiency()
-    plot_eta_ratio_minus_one()
+    plot_lim_eta_ratio()
+    #plot_eta_ratio()
+    #plot_beta_ratio()
+
+    """
+    theta1 = 0.01
+    theta2 = 0.01
+    np_beta = compute_np_beta(alpha, theta1, theta2)
+    cp_beta = compute_cp_beta(alpha, theta1, theta2)
+    np_eta = compute_np_eta(alpha, theta1, theta2, np_beta)
+    cp_eta = compute_cp_eta(alpha, theta1, theta2, cp_beta)
+    np_eff = compute_efficiency(n, alpha, np_eta, np_beta)
+    cp_eff = compute_efficiency(n, alpha, cp_eta, cp_beta)
+
+
+    print('np_beta, cp_beta', np_beta, cp_beta)
+    print('cp_eff - np_eff', cp_eff - np_eff)
+    print('cp_eta / np_eta', cp_eta / np_eta)
+    """
+    
